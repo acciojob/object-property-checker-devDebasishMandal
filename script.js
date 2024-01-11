69e0524570ce2b634132ea59bdb134834eae90d6
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-01-11 16:00:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let d=false;
+	let x= Object.keys(sampleObject);
+	x.forEach((e)=>{
+		if(e===key){
+			d=true
+		}
+	});
+	return d;
 }
 
 // Do not change the code below
